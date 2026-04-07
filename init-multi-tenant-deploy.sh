b89b2edbc294c5eaec7c9982a30bdf5ec67b377c
Yungang <solariswu@hotmail.com>
2026-04-07 14:52:20 +1000

    Create init-multi-tenant-deploy.sh

---
+++ init-multi-tenant-deploy.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+#project code repo
+REPO_BASE=https://github.com/kcsapersona/
+export APERSONAIDP_REPO_NAME=aPersona-Identity_Multi-Tenant_Source
+
+#install NODE NPM GIT
+NVM_VER=v0.39.7
+NPM_VER=11.0.0
+
+rm -rf .nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/$NVM_VER/install.sh | bash
+source ~/.bashrc
+echo "install nvm "$NVM_VER
+nvm install --lts >/dev/null
+ 
+ #install git
+echo "install git"
+sudo yum update -y >/dev/null
+sudo yum install git -y >/dev/null
+ 
+ #update npm
+echo "install npm v"$NPM_VER
+npm install -g npm@$NPM_VER >/dev/null
+npm install -g aws-cdk >/dev/null
+
+#download code repo
+echo "clone the repo $REPO_BASE$APERSONAIDP_REPO_NAME"
+git clone $REPO_BASE$APERSONAIDP_REPO_NAME
+
+cd $APERSONAIDP_REPO_NAME
+git pull
